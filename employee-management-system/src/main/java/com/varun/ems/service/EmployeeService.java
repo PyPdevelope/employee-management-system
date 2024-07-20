58f319fc79649bdf5ff614d978fa9f8206f6dc37
PyPdevelope <86388596+PyPdevelope@users.noreply.github.com>
2024-07-20 18:26:08 +0530

    Add files via upload

---
+++ employee-management-system/src/main/java/com/varun/ems/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.varun.ems.service;
+
+import com.varun.ems.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(Long id);
+    void deleteEmployeeById(Long id);
+}
